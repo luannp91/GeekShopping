89203aeede054bde76da039d662ba0229d93cfe0
Luan Pinto <luan.nelmo@hotmail.com>
2023-06-15 21:36:03 -0300

    Entendendo e Implementando a Classe HttpClientExtensions

---
+++ GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace GeekShopping.Web.Utils
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException($"Something went wrong calling the API: " +
+                    $"{response.ReasonPhrase}");
+            }
+            var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+#pragma warning disable CS8603 // Possível retorno de referência nula.
+            return JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+#pragma warning restore CS8603 // Possível retorno de referência nula.
+        }
+    }
+}
