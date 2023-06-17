b3c078534c8792f4393fa6630a4738aca4b94e5c
Luan Pinto <luan.nelmo@hotmail.com>
2023-06-17 17:27:26 -0300

    Injeção de Dependência da Classe ProductService na Classe Program

--- GeekShopping.Web/Services/ProductService.cs
+++ GeekShopping.Web/Services/ProductService.cs
@@ -9,6 +9,11 @@ namespace GeekShopping.Web.Services
         private readonly HttpClient? _client;
         public const string BasePath = "api/v1/product";
 
+        public ProductService(HttpClient? client)
+        {
+            _client = client;
+        }
+
         public async Task<IEnumerable<ProductModel>> FindAllProducts()
         {
             var response = await _client!.GetAsync(BasePath);
@@ -21,19 +26,43 @@ namespace GeekShopping.Web.Services
             return await response.ReadContentAs<ProductModel>();
         }
 
-        public Task<ProductModel> CreateProduct(ProductModel model)
+        public async Task<ProductModel> CreateProduct(ProductModel model)
         {
-            throw new NotImplementedException();
+            var response = await _client!.PostAsJson(BasePath, model);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.ReadContentAs<ProductModel>();
+            }
+            else
+            {
+                throw new Exception("Something went wrong when calling API");
+            }
         }
 
-        public Task<ProductModel> UpdateProduct(ProductModel model)
+        public async Task<ProductModel> UpdateProduct(ProductModel model)
         {
-            throw new NotImplementedException();
+            var response = await _client!.PutAsJson(BasePath, model);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.ReadContentAs<ProductModel>();
+            }
+            else
+            {
+                throw new Exception("Something went wrong when calling API");
+            }
         }
 
-        public Task<bool> DeleteProductById(long id)
+        public async Task<bool> DeleteProductById(long id)
         {
-            throw new NotImplementedException();
+            var response = await _client!.DeleteAsync($"{BasePath}/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.ReadContentAs<bool>();
+            }
+            else
+            {
+                throw new Exception("Something went wrong when calling API");
+            }
         }
     }
 }
