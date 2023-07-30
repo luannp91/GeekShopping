a0ed63dd6ed3d7667916f87fec63a1333b855d48
Luan Pinto <luan.nelmo@hotmail.com>
2023-07-30 16:55:05 -0300

    Implementando o Controller de Produtos

---
+++ GeekShopping.Web/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService? _productService;
+
+        public ProductController(IProductService? productService)
+        {
+            _productService = productService ?? throw new ArgumentNullException(nameof(productService));
+        }
+
+#pragma warning disable CS1998 // O método assíncrono não possui operadores 'await' e será executado de forma síncrona
+        public async Task<IActionResult> ProductIndex()
+#pragma warning restore CS1998 // O método assíncrono não possui operadores 'await' e será executado de forma síncrona
+        {
+            var products = _productService!.FindAllProducts();
+            return View(products);
+        }
+    }
+}
