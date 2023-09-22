39481b756993e23b8d33266c5bd038d54332a101
Luan Pinto <luan.nelmo@hotmail.com>
2023-09-22 20:41:15 -0300

    Criando a classe responsável por inicializar o banco de dados

---
+++ GeekShopping.IdentityServer/Initializer/DbInitializer.cs
@@ -0,0 +1,30 @@
+﻿using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+
+namespace GeekShopping.IdentityServer.Initializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly MySQLContext? _context;
+        private readonly UserManager<ApplicationUser>? _userManager;
+        private readonly RoleManager<ApplicationUser>? _roleManager;
+
+        public DbInitializer(MySQLContext? context, UserManager<ApplicationUser>? userManager, RoleManager<ApplicationUser>? roleManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public void Initialize()
+        {
+            if (_roleManager!.FindByNameAsync(IdentityConfiguration.Admin).Result != null) 
+            {
+                _roleManager.CreateAsync(new IdentityRole(IdentityConfiguration.Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(IdentityConfiguration.Client)).GetAwaiter().GetResult();
+            }
+        }
+    }
+}
